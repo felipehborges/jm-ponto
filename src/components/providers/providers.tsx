f15b6fba0526380240a5e07216cc77c0166baef1
Hamasaki <felipehama@gmail.com>
2025-02-24 16:01:07 -0300

    Fix Providers

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -6,13 +6,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
-  const [mounted, setMounted] = useState(false)
+  // const [mounted, setMounted] = useState(false)
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+  // useEffect(() => {
+  //   setMounted(true)
+  // }, [])
 
-  if (!mounted) return <>{children}</>
+  // if (!mounted) return <>{children}</>
 
   return (
     <QueryClientProvider client={queryClient}>
