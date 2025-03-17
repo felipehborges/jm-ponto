a04b00f50c4e74a888ad2a4fd87ac3707462f491
Hamasaki <felipehama@gmail.com>
2025-03-17 17:03:58 -0300

    Fix Employee Details layout

--- src/app/admin/employees/[id]/employee-attendances-table/table.tsx
+++ src/app/admin/employees/[id]/employee-attendances-table/table.tsx
@@ -44,78 +44,70 @@ export function EmployeeAttendancesTable<TData, TValue>({
   })
 
   return (
-    <div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id} className="text-center py-2">
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="text-center py-2">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+                className="text-center"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableHeader>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                Sem resultados.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
 
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                  className="text-center"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Sem resultados.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+      <div className="flex items-center justify-end space-x-2 p-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Anterior
+        </Button>
 
-        <div className="flex items-center justify-end space-x-2 p-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Anterior
-          </Button>
-
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Próximo
-          </Button>
-        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Próximo
+        </Button>
       </div>
     </div>
   )
--- src/app/admin/employees/[id]/page.tsx
+++ src/app/admin/employees/[id]/page.tsx
@@ -38,104 +38,102 @@ export default async function EmployeeDetails({
   // })
 
   return (
-    <div className="relative flex flex-col gap-4 overflow-hidden mb-10">
+    <div className="relative flex flex-col gap-4 mb-10">
       <BackButton />
 
-      <section className="w-full flex flex-col my-4 gap-6">
-        <div className="flex justify-center gap-4 m-4">
-          <div className="w-80 flex justify-center items-center">
-            <Image
-              src={employee.imgUrl}
-              alt="employee"
-              width={220}
-              height={220}
-              className="rounded-lg shadow-md border-2 border-secondary"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2 p-2 mt-4">
-            <div className="flex gap-2">
-              <strong>Nome: </strong>
-              <p>{employee.name}</p>
+      <section className="w-full flex flex-col items-center my-4 gap-6">
+        <div>
+          <div className="flex flex-col items-center pb-5 lg:flex-row lg:justify-start">
+            <div className="w-60 flex justify-center items-center">
+              <Image
+                src={employee.imgUrl}
+                alt="employee"
+                width={220}
+                height={220}
+                className="rounded-lg shadow-md border-2 border-secondary"
+              />
             </div>
 
-            <div className="flex gap-2">
-              <strong>Cargo: </strong>
-              <p>{employee.position}</p>
-            </div>
-
-            <div className="flex gap-2">
-              <strong>Cartão RFID: </strong>
-              <p>{employee.rfid}</p>
-            </div>
-
-            <div className="flex gap-2">
-              <strong>ID: </strong>
-              <p>{employee.id}</p>
-            </div>
-
-            <div className="w-full mt-4 flex justify-center">
-              <EditEmployeeButton employee={employee} />
+            <div className="flex flex-col items-start gap-2 p-2 ml-6 mt-4">
+              <div className="flex gap-2">
+                <strong>Nome: </strong>
+                <p>{employee.name}</p>
+              </div>
+              <div className="flex gap-2">
+                <strong>Cargo: </strong>
+                <p>{employee.position}</p>
+              </div>
+              <div className="flex gap-2">
+                <strong>Cartão RFID: </strong>
+                <p>{employee.rfid}</p>
+              </div>
+              <div className="flex gap-2">
+                <strong>ID: </strong>
+                <p>{employee.id}</p>
+              </div>
+              <div className="w-full mt-4 flex justify-center">
+                <EditEmployeeButton employee={employee} />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="gap-2 lg:gap-4 flex justify-center items-center">
-          <Card className="size-28 sm:size-38 md:size-45 lg:size-60 flex flex-col">
-            <CardHeader className="p-4 lg:p-6">
-              <CardTitle className="text-center text-sm md:text-base lg:text-lg">
-                Horas trabalhadas
-              </CardTitle>
-            </CardHeader>
+          <div className="gap-2 lg:gap-4 flex justify-center items-center ">
+            <Card className="size-28 sm:size-36 md:size-45 lg:size-60 flex flex-col border-2">
+              <CardHeader className="p-4 lg:p-6">
+                <CardTitle className="text-center text-sm md:text-base lg:text-lg">
+                  Horas trabalhadas
+                </CardTitle>
+              </CardHeader>
 
-            <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
-              {/* TODO: Estático */}
-              34:12
-              {/* {formatSeconds(report?.totalWorkedHours || 0)} */}
-            </CardContent>
-          </Card>
+              <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
+                {/* TODO: Estático */}
+                34:12
+                {/* {formatSeconds(report?.totalWorkedHours || 0)} */}
+              </CardContent>
+            </Card>
 
-          <Card className="size-28 sm:size-38 md:size-45 lg:size-60 flex flex-col">
-            <CardHeader className="p-4 lg:p-6">
-              <CardTitle className="text-center text-sm md:text-base lg:text-lg">
-                Horas extras
-              </CardTitle>
-            </CardHeader>
+            <Card className="size-28 sm:size-36 md:size-45 lg:size-60 flex flex-col border-2">
+              <CardHeader className="p-4 lg:p-6">
+                <CardTitle className="text-center text-sm md:text-base lg:text-lg">
+                  Horas extras
+                </CardTitle>
+              </CardHeader>
 
-            <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
-              {/* TODO: Estático */}
-              12:34
-              {/* {formatSeconds(reports?.totalOvertime || 0)} */}
-            </CardContent>
-          </Card>
+              <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
+                {/* TODO: Estático */}
+                12:34
+                {/* {formatSeconds(reports?.totalOvertime || 0)} */}
+              </CardContent>
+            </Card>
 
-          <Card className="size-28 sm:size-38 md:size-45 lg:size-60 flex flex-col">
-            <CardHeader className="p-4 lg:p-6">
-              <CardTitle className="text-center text-sm md:text-base lg:text-lg">
-                Atrasos
-              </CardTitle>
-            </CardHeader>
+            <Card className="size-28 sm:size-36 md:size-45 lg:size-60 flex flex-col border-2">
+              <CardHeader className="p-4 lg:p-6">
+                <CardTitle className="text-center text-sm md:text-base lg:text-lg">
+                  Atrasos
+                </CardTitle>
+              </CardHeader>
 
-            <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
-              {/* TODO: Estático */}
-              34
-              {/* {formatSeconds(reports?.totalDelay || 0)} */}
-            </CardContent>
-          </Card>
+              <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
+                {/* TODO: Estático */}
+                34
+                {/* {formatSeconds(reports?.totalDelay || 0)} */}
+              </CardContent>
+            </Card>
 
-          <Card className="size-28 sm:size-38 md:size-45 lg:size-60 flex flex-col">
-            <CardHeader className="p-4 lg:p-6">
-              <CardTitle className="text-center text-sm md:text-base lg:text-lg">
-                Faltas
-              </CardTitle>
-            </CardHeader>
+            <Card className="size-28 sm:size-36 md:size-45 lg:size-60 flex flex-col border-2">
+              <CardHeader className="p-4 lg:p-6">
+                <CardTitle className="text-center text-sm md:text-base lg:text-lg">
+                  Faltas
+                </CardTitle>
+              </CardHeader>
 
-            <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
-              {/* TODO: Estático */}
-              12
-              {/* {reports?.daysAbsences.length} */}
-            </CardContent>
-          </Card>
+              <CardContent className="flex justify-center items-center text-base sm:text-xl md:text-3xl lg:text-5xl size-full">
+                {/* TODO: Estático */}
+                12
+                {/* {reports?.daysAbsences.length} */}
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </section>
 
